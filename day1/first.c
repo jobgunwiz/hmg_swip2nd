9a8f640315973b6d34ca89cbbeb74ea78a76c7b7
Hyeonggeon Lee <50469718+jobgunwiz@users.noreply.github.com>
2022-01-21 10:25:35 +0900

    first exercise
    first file for this class!

---
+++ day1/first.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//start point!
+//os gives resources -> load class.exe content to memory and
+//call main(void)
+int main()
+{
+	//printf("hello\n");
+
+	return 0;
+	//return to os : value 0
+	//resource unlock(?!)
+	//
+	//someone who call this program that i am '0' - not error
+}
+
+//ctrl+shift+b : build,, same as 'solution build'
+//ctrl + f5 = run!
+
+//in folder -> first.obj file
+//each c file for each obj file <- by compiler
+// & linker
+//and then, make one running file
