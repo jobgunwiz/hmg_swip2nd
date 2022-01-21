2bcdbf644aac99fc1f80a300d9ad39ecc88bdf43
Hyeonggeon Lee <50469718+jobgunwiz@users.noreply.github.com>
2022-01-21 11:42:23 +0900

    comment
    and something else

---
+++ day1/first_command_etc.c
@@ -0,0 +1,48 @@
+/*
+this code is made by hg_lee, 22-01-21 @ hmg ngv c lecture
+*/
+
+#include <stdio.h>
+//made by someone else...?!
+//include to this project file
+//if include "~~" : made by user
+
+//program start point!
+//os gives resources -> load class.exe content to memory and
+//call main(void)
+int main()
+{
+	int a;
+	
+	a = printf("hello, world\n");
+	//stdio declared// function and variable
+
+	printf("ret : %d    -\n", a);
+	//return means number of charactor that displayed on display!
+	//so function (not always) have input and output
+	
+	//d means '¼­½Ä' and it is for integer
+	//variation d x o s c f p... etc...
+
+	//  \n means 'escape sequence'.. for line skipping
+	//  variation ' " ... sth like that
+
+	return 0;
+	//return to os : value 0
+	//resource unlock(?!)
+	//
+	//someone who call this program that i am '0' - not error
+
+	// and '//' means caption .. cannot read by compiler
+	//ctrl k c // ctrl k u
+	//it gives info for other people
+
+}
+
+//ctrl+shift+b : build,, same as 'solution build'
+//ctrl + f5 = run!
+
+//in folder -> first.obj file
+//each c file for each obj file <- by compiler
+// & linker
+//and then, make one running file
