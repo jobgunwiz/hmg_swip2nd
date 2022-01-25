4f9326b839eeb87ff8d16bba6bb80bc0e27db8ce
Hyeonggeon Lee <50469718+jobgunwiz@users.noreply.github.com>
2022-01-25 14:05:45 +0900

    Update fn_to_pt.c

--- day3/fn_to_pt.c
+++ day3/fn_to_pt.c
@@ -6,14 +6,16 @@ void stst(int pt)
 {
 	static int hello = 300;
 
-	//printf("###%d", pt);
+	printf("3. %d\n", pt);
 	int* pt_addr = pt;
 
-	printf("%d\n", pt);
+	printf("4. %d\n", *pt_addr);
+	printf("4-1 %d\n", pt_addr);
 
 	*pt_addr = &hello;
 
-	printf("%d\n", pt);
+	printf("5. %d\n", *pt_addr);
+	printf("5-1. %d\n", pt_addr);
 }
 
 int main()
@@ -21,12 +23,13 @@ int main()
 	int aa = 10;
 	int* pointer = &aa;
 	
-	//printf("%d\n", &pointer);
-	printf("%d\n", *pointer);
+	printf("1. %d\n", &pointer);
+	printf("2. %d\n", *pointer);
+	printf("2-1. %d\n", pointer);
 
 	stst(&pointer);
 
-	printf("%d\n", *pointer);
+	printf("6. %d\n", *pointer);
 
 	return 0;
 }
