080835831597df3b1876a0899f41df486210dc86
Hyeonggeon Lee <50469718+jobgunwiz@users.noreply.github.com>
2022-01-26 14:47:51 +0900

    Update fn_pt_hard.c

--- day4/fn_pt_hard.c
+++ day4/fn_pt_hard.c
@@ -32,12 +32,32 @@ int recv_fn_pointer(int (*f)(int, int), int num)
 
 }
 
+
+//int (*f)(int, int) -> int (*fn(param))(int, int)
 //how about return??
 int (*return_fn_pointer(int num))(int, int)
 {
 	printf("called\n");
 	return &add;
 	//return_type (*name(param1, param2))()
+}
+//but usually typedef
+
+typedef int int_my_32bit;
+typedef int FUNC(int, int);
+//fn point nickname
+
+//usually like this
+FUNC* myfunc(int a, int b)
+{
+
+}
+
+//or more shorter
+typedef int (*PFUNC)(int, int);
+PFUNC myfunc2(int a, int b)
+{
+
 }
 
 int main()
